2d68536e3b58d935f767f2563fb12088ba068c8b
Pascal Eberlein <pascal@eberlein.io>
2024-12-04 00:30:04 +0100

    don't clone submodules

--- cmake/AddOpenSSL.cmake
+++ cmake/AddOpenSSL.cmake
@@ -1,11 +1,13 @@
 include(ExternalProject)
 
+include(cmake/AddZLib.cmake)
+
 if(${TOOLCHAIN_PREFIX} MATCHES "mingw32-$")
     set(CONFIGURE_PREFIX -m32 mingw)
 elseif(${TOOLCHAIN_PREFIX} MATCHES "mingw64-$")
     set(CONFIGURE_PREFIX mingw64)
 else()
-    set(CONFIGURE_PREFIX linux-generic32)
+    set(CONFIGURE_PREFIX linux-elf)
 endif()
 message("OpenSSL configure prefix: ${CONFIGURE_PREFIX}")
 
@@ -16,17 +18,19 @@ ExternalProject_Add(openssl
         GIT_REPOSITORY https://github.com/openssl/openssl
         GIT_PROGRESS 1
         GIT_TAG OpenSSL_1_0_2j
+        GIT_SUBMODULES ""
         UPDATE_DISCONNECTED 1
         BUILD_IN_SOURCE 1
         CONFIGURE_COMMAND
-        ./Configure ${CONFIGURE_PREFIX} no-shared no-asm
+        ./Configure ${CONFIGURE_PREFIX} threads no-shared no-asm no-tests
         --cross-compile-prefix=${TOOLCHAIN_PREFIX}
         --prefix=${OPENSSL_BINARY_DIR}
         -static -static-libgcc -fPIC
         BUILD_COMMAND make clean && make
         INSTALL_COMMAND make install
-        )
+)
 
+add_dependencies(openssl zlib)
 ExternalProject_Get_Property(openssl SOURCE_DIR)
 set(OPENSSL_SOURCE_PATH ${SOURCE_DIR}/src)
 include_directories(${OPENSSL_SOURCE_PATH})
