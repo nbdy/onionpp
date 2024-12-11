5fd1e0bd5e57f22b1c44d27a5ead1e29d95d819d
Pascal Eberlein <pascal@eberlein.io>
2024-12-11 14:37:28 +0100

    fix for windows

--- test_load_so.cpp
+++ test_load_so.cpp
@@ -3,8 +3,13 @@
 //
 
 #include <filesystem>
-#include <dlfcn.h>
 #include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <dlfcn.h>
+#endif
 
 #include "onionpp/Tor/ITor.h"
 
@@ -12,19 +17,62 @@ typedef onionpp::ITor* (*TorFactory)();
 
 int main() {
   std::cout << "Current path: " << std::filesystem::current_path() << std::endl;
+
+#ifdef _WIN32
+  const auto path = std::filesystem::current_path() / "libonionpp-shared.dll";
+#else
   const auto path = std::filesystem::current_path() / "libonionpp-shared.so";
-  std::cout << "Loading library: " << path.c_str() << std::endl;
-  void *handle = dlopen(path.c_str(), RTLD_LAZY);
+#endif
+  const std::string pathStr = path.string();
+
+  std::cout << "Loading library: " << pathStr << std::endl;
+#ifdef _WIN32
+  HMODULE handle = LoadLibrary(pathStr.c_str());
   if (handle == nullptr) {
-    std::cout << "Could not load library: " << dlerror() << std::endl;
+    std::cerr << "Could not load library: " << GetLastError() << std::endl;
     return 1;
   }
+#else
+  void* handle = dlopen(path.c_str(), RTLD_LAZY);
+  if (handle == nullptr) {
+    std::cerr << "Could not load library: " << dlerror() << std::endl;
+    return 1;
+  }
+#endif
+
   std::cout << "Loading create_tor symbol" << std::endl;
-  const auto factory = reinterpret_cast<TorFactory>(dlsym(handle, "create_tor"));
+#ifdef _WIN32
+  auto factory = reinterpret_cast<TorFactory>(GetProcAddress(handle, "create_tor"));
+  if (factory == nullptr) {
+    std::cerr << "Could not load symbol: " << GetLastError() << std::endl;
+    FreeLibrary(handle);
+    return 1;
+  }
+#else
+  auto factory = reinterpret_cast<TorFactory>(dlsym(handle, "create_tor"));
+  if (factory == nullptr) {
+    std::cerr << "Could not load symbol: " << dlerror() << std::endl;
+#ifdef _WIN32
+    FreeLibrary(handle);
+#else
+    dlclose(handle);
+#endif
+    return 1;
+  }
+#endif
+
   std::cout << "Creating tor instance" << std::endl;
   onionpp::ITor *tor = factory();
+
   std::cout << "Starting tor instance" << std::endl;
   tor->start(true);
+
+  std::cout << "Done. Cleaning up" << std::endl;
+#ifdef _WIN32
+  FreeLibrary(handle);
+#else
+  dlclose(handle);
+#endif
 
   return 0;
 }
