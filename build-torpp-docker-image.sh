9add440fe0498fd7e484509cd83bdfec9750f896
Pascal Eberlein <pascal@eberlein.io>
2024-12-22 17:29:37 +0100

    tag image with version and latest

--- build-torpp-docker-image.sh
+++ build-torpp-docker-image.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 IMAGE_NAME="torpp"
-IMAGE_VERSION="latest"
+IMAGE_VERSION="1.0.1"
 DOCKERFILE_PATH="."
-EXPORT_PATH="./torpp-image.tar"
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -32,26 +31,31 @@ while [[ $# -gt 0 ]]; do
 done
 
 FULL_IMAGE_NAME="${IMAGE_NAME}:${IMAGE_VERSION}"
+EXPORT_PATH="./${FULL_IMAGE_NAME}.tar"
 
-if docker images --format '{{.Repository}}:{{.Tag}}' | grep -wq "$FULL_IMAGE_NAME"; then
-  echo "Docker image '$FULL_IMAGE_NAME' already exists."
+if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q "${IMAGE_NAME}:${IMAGE_VERSION}"; then
+    echo "Docker image '${IMAGE_NAME}:${IMAGE_VERSION}' already exists. Skipping build."
 else
-  echo "Docker image '$FULL_IMAGE_NAME' does not exist. Building it now..."
-  docker build -t "$FULL_IMAGE_NAME" "$DOCKERFILE_PATH"
+    echo "Docker image '${IMAGE_NAME}:${IMAGE_VERSION}' does not exist. Building now..."
+    docker build -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
+    if [ $? -ne 0 ]; then
+      echo "Failed to build Docker image '$FULL_IMAGE_NAME'. Check the Docker logs for more information."
+      exit 1
+    fi
 
-  if [ $? -ne 0 ]; then
-    echo "Failed to build Docker image '$FULL_IMAGE_NAME'. Check the Docker logs for more information."
-    exit 1
-  fi
-  echo "Docker image '$FULL_IMAGE_NAME' built successfully."
+    echo "Tagging the image with the 'latest' tag..."
+    docker tag "${IMAGE_NAME}:${IMAGE_VERSION}" "${IMAGE_NAME}:latest"
+    echo "Docker image '${IMAGE_NAME}:${IMAGE_VERSION}' built successfully with 'latest' tag."
 fi
 
-echo "Exporting Docker image '$FULL_IMAGE_NAME' to '$EXPORT_PATH'..."
-docker save -o "$EXPORT_PATH" "$FULL_IMAGE_NAME"
+if [ ! -f ${EXPORT_PATH} ]; then
+  echo "Exporting Docker image '$FULL_IMAGE_NAME' to '$EXPORT_PATH'..."
+  docker save -o "$EXPORT_PATH" "$FULL_IMAGE_NAME"
 
-if [ $? -eq 0 ]; then
-  echo "Docker image '$FULL_IMAGE_NAME' exported successfully to '$EXPORT_PATH'."
-else
-  echo "Failed to export Docker image '$FULL_IMAGE_NAME'."
-  exit 1
+  if [ $? -eq 0 ]; then
+    echo "Docker image '$FULL_IMAGE_NAME' exported successfully to '$EXPORT_PATH'."
+  else
+    echo "Failed to export Docker image '$FULL_IMAGE_NAME'."
+    exit 1
+  fi
 fi
